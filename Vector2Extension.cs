66f8076eace2bd5d97a2a0651ad15f2e14561217
Martin <martin.grzeslowski@gmail.com>
2021-06-17 17:08:20 +0200

    Create Vector2Extension.cs

---
+++ Vector2Extension.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class Vector2Extension
+{
+    /// <summary>
+    /// http://answers.unity.com/answers/734946/view.html
+    /// </summary>
+    public static Vector2 Rotate(this Vector2 v, float degrees)
+    {
+        var sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        var cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        var tx = v.x;
+        var ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+
+        return v;
+    }
+}
