02844d75457ed0e9f51a8f528766f6fe97fd19d6
Martin <martin.grzeslowski@gmail.com>
2021-06-17 17:13:28 +0200

    Create ExitGame.cs

---
+++ ExitGame.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics.CodeAnalysis;
+using UnityEditor;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Used in Unity")]
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
