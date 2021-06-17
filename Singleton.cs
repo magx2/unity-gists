a90b465f8ae3697b27414efa3939c8da0cd66d4e
Martin <martin.grzeslowski@gmail.com>
2021-06-17 17:02:18 +0200

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    protected static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = (T) this;
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.LogError($"Instance of [{name}] already exists!");
+#endif
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        if (Instance != this) return;
+        OnDestroySingleton();
+        Instance = null;
+    }
+
+    protected virtual void OnDestroySingleton()
+    {
+            
+    }
+}
