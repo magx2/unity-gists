75080b0a10fbd2a95b711be626677e7d31796212
Martin <martin.grzeslowski@gmail.com>
2021-06-20 11:02:03 +0200

    Create RotateConstantly.cs

---
+++ RotateConstantly.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RotateConstantly : MonoBehaviour
+{
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(1, 1, 1);
+
+    [Header("Random Directions")] [SerializeField]
+    private bool randomDirectionX = true;
+
+    [SerializeField] private bool randomDirectionY = true;
+    [SerializeField] private bool randomDirectionZ = true;
+
+    private void Awake()
+    {
+        if (randomDirectionX) rotationSpeed.x *= Mathf.Sign(Random.Range(-1, 1));
+        if (randomDirectionY) rotationSpeed.y *= Mathf.Sign(Random.Range(-1, 1));
+        if (randomDirectionZ) rotationSpeed.z *= Mathf.Sign(Random.Range(-1, 1));
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(rotationSpeed* Time.fixedDeltaTime, Space.Self);
+    }
+}
