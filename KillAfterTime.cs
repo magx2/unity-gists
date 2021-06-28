79d1a2c8c3f7f6720706d9116bd4d7d2a7eb2987
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2021-06-28 23:34:00 +0200

    new misc classes

---
+++ KillAfterTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class KillAfterTime : MonoBehaviour
+    {
+        [Tooltip("After what time kill the game object")] [SerializeField] [Min(0.1f)]
+        private float ttl = 1;
+
+        private void Start()
+        {
+            Destroy(gameObject, ttl);
+        }
+    }
+}
---
+++ MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class MoveForward : MonoBehaviour
+    {
+        [SerializeField] [Min(0.1f)] private float speed = 1;
+
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.MovePosition(_rigidbody.position + transform.right * (speed * Time.fixedDeltaTime));
+        }
+    }
+}
