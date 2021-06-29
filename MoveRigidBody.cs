52a907a681a0b44861877bd19a45901b1c37e653
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2021-06-29 16:39:08 +0200

    Add MoveRigidBody

---
+++ MoveRigidBody.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class MoveRigidBody : MonoBehaviour
+    {
+        [SerializeField] private Vector3 speed = new Vector3(1, 1, 1);
+
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            var deltaSpeed = speed * Time.fixedDeltaTime;
+            var thisTransform = transform;
+            _rigidbody.MovePosition(_rigidbody.position
+                                    + thisTransform.right * deltaSpeed.x
+                                    + thisTransform.up * deltaSpeed.y
+                                    + thisTransform.forward * deltaSpeed.z);
+        }
+    }
+}
