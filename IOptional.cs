4b6c6eb98d36f7bca8a614460beb0522519882f1
Martin <martin.grzeslowski@gmail.com>
2021-07-26 22:27:10 +0200

    Add `MonoBehaviourOptional`

--- IOptional.cs
+++ IOptional.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
 
 namespace Misc
 {
@@ -21,17 +23,12 @@ namespace Misc
     {
         private readonly T _value;
 
-        public static Something<T> Of(T value)
-        {
-            return new Something<T>(value);
-        }
-        
         private Something(T value)
         {
             if (value == null) throw new NullReferenceException("Value was null!");
             _value = value;
         }
-        
+
         public bool IsEmpty()
         {
             return false;
@@ -61,19 +58,19 @@ namespace Misc
         {
             action.Invoke(_value);
         }
+
+        public static Something<T> Of(T value)
+        {
+            return new Something<T>(value);
+        }
     }
 
     public class Nothing<T> : IOptional<T>
     {
-        public static Nothing<T> Of()
-        {
-            return new Nothing<T>();
-        }
-
         private Nothing()
         {
         }
-        
+
         public bool IsEmpty()
         {
             return true;
@@ -102,6 +99,121 @@ namespace Misc
         public void IfPresent(Action<T> action)
         {
             // do nothing
+        }
+
+        public static Nothing<T> Of()
+        {
+            return new Nothing<T>();
+        }
+    }
+
+    public class RuntimeOptional<T> : IOptional<T>
+    {
+        private readonly T _value;
+
+        private RuntimeOptional(T value)
+        {
+            _value = value;
+        }
+
+        public bool IsEmpty()
+        {
+            return _value == null;
+        }
+
+        public bool IsNotEmpty()
+        {
+            return _value != null;
+        }
+
+        public T Value()
+        {
+            if (_value == null) throw new NullReferenceException("Value was null!");
+            return _value;
+        }
+
+        [SuppressMessage("ReSharper", "RedundantIfElseBlock")]
+        [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
+        public T OrElse(T other)
+        {
+            if (_value != null)
+                return _value;
+            else
+                return other;
+        }
+
+        [SuppressMessage("ReSharper", "RedundantIfElseBlock")]
+        public IOptional<TOut> Map<TOut>(Func<T, TOut> mapper)
+        {
+            if (_value != null)
+                return new RuntimeOptional<TOut>(mapper.Invoke(_value));
+            else
+                return Nothing<TOut>.Of();
+        }
+
+        public void IfPresent(Action<T> action)
+        {
+            if (_value != null) action.Invoke(_value);
+        }
+
+        public static RuntimeOptional<T> Of(T value)
+        {
+            return new RuntimeOptional<T>(value);
+        }
+    }
+
+    public class MonoBehaviourOptional<TMono> : IOptional<TMono> where TMono : MonoBehaviour
+    {
+        private readonly TMono _value;
+
+        private MonoBehaviourOptional(TMono value)
+        {
+            _value = value;
+        }
+
+        public bool IsEmpty()
+        {
+            return _value == null;
+        }
+
+        public bool IsNotEmpty()
+        {
+            return _value != null;
+        }
+
+        public TMono Value()
+        {
+            if (_value == null) throw new NullReferenceException("Value was null!");
+            return _value;
+        }
+
+        [SuppressMessage("ReSharper", "RedundantIfElseBlock")]
+        [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
+        public TMono OrElse(TMono other)
+        {
+            if (_value != null)
+                return _value;
+            else
+                return other;
+        }
+
+        [SuppressMessage("ReSharper", "RedundantIfElseBlock")]
+        public IOptional<TOut> Map<TOut>(Func<TMono, TOut> mapper)
+        {
+            if (_value != null)
+                return RuntimeOptional<TOut>.Of(mapper.Invoke(_value));
+            else
+                return Nothing<TOut>.Of();
+        }
+
+        public void IfPresent(Action<TMono> action)
+        {
+            if (_value != null) action.Invoke(_value);
+        }
+
+        public static MonoBehaviourOptional<TMono> Of(TMono value)
+        {
+            return new MonoBehaviourOptional<TMono>(value);
         }
     }
 }
