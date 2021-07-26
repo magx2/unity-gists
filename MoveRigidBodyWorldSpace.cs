fe3f94b8f3959c2cfc8b329117bdc01a3fd29fa0
Martin <martin.grzeslowski@gmail.com>
2021-07-26 20:44:22 +0200

    Create MoveRigidBodyWorldSpace.cs

---
+++ MoveRigidBodyWorldSpace.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Misc
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class MoveRigidBodyWorldSpace : MonoBehaviour
+    {
+        [SerializeField] private Vector3 speed = new Vector3(1, 1, 1);
+
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            var deltaSpeed = speed * Time.fixedDeltaTime;
+            _rigidbody.MovePosition(_rigidbody.position + deltaSpeed);
+        }
+    }
+}
