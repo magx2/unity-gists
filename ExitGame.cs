b1f42ab671d62439429aff7184c2f4c1d5677e4e
Martin <martin.grzeslowski@gmail.com>
2021-08-01 23:16:47 +0200

    Change `ExitGame` to singleton

--- ExitGame.cs
+++ ExitGame.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 namespace Misc
 {
-    public class ExitGame : MonoBehaviour
+    public class ExitGame : Singleton<ExitGame>
     {
+        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Used in Unity")]
+        public static void InvokeExit()
+        {
+            Instance.Exit();
+        }
+        
         [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Used in Unity")]
         public void Exit()
         {
