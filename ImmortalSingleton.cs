bbbdbe11cefd9e399f77a0e6eb41c231bab2ca0a
Martin <martin.grzeslowski@gmail.com>
2021-08-02 00:09:20 +0200

    Create ImmortalSingleton.cs

---
+++ ImmortalSingleton.cs
@@ -0,0 +1,15 @@
+namespace Misc
+{
+    /// <summary>
+    /// This singleton will not be destroyed between loading scenes
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ImmortalSingleton<T> : Singleton<T> where T : ImmortalSingleton<T>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
