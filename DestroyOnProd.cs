4374ca943c76e8029a378b9370b93e7895c2389b
Martin <martin.grzeslowski@gmail.com>
2021-09-19 19:24:29 +0200

    Turn off `pragma 414`

--- DestroyOnProd.cs
+++ DestroyOnProd.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 namespace Misc
 {
     public class DestroyOnProd : MonoBehaviour
     {
-        [SerializeField] private bool destroyOnProd = true;
+#pragma warning disable 414
+        [SerializeField] [SuppressMessage("ReSharper", "NotAccessedField.Local", Justification = "It's used in prod")]
+        private bool destroyOnProd = true;
+#pragma warning restore 414
 
         private void Awake()
         {
