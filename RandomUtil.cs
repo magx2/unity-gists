9b0b108b8ff90b75c5262322250da7d75705cbee
Martin <martin.grzeslowski@gmail.com>
2021-10-05 22:39:29 +0200

    Update RandomUtil.cs

--- RandomUtil.cs
+++ RandomUtil.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
 
 namespace Misc
 {
@@ -39,5 +41,27 @@ namespace Misc
         }
 
         #endregion
+
+        /// <summary>
+        /// This method takes random value between [0,1)
+        /// and return true if it's bigger or equals than given threshold.
+        /// </summary>
+        /// <param name="treshold">Threshold to check randomly generated number</param>
+        /// <returns>
+        /// true if random double is bigger or equals given threshold
+        /// false otherwise
+        /// </returns>
+        public static bool OverRandomTreshold(float treshold)
+        {
+#if UNITY_EDITOR
+            if (treshold > 1)
+                Debug.LogWarning($"Treshold [{treshold}] is bigger than 1!" +
+                                 "This method will always return `false`!");
+            if (treshold < 0)
+                Debug.LogWarning($"Treshold [{treshold}] is lower than 0!" +
+                                 "This method will always return `true`!");
+#endif
+            return Random.NextDouble() >= treshold;
+        }
     }
 }
