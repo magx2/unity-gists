ee56018b7eb49e5addf236cccb0e97b99343835e
Martin <martin.grzeslowski@gmail.com>
2021-10-19 22:01:37 +0200

    Create Immortal.cs

---
+++ Immortal.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Misc
+{
+    public class Immortal : MonoBehaviour
+    {
+        protected void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
